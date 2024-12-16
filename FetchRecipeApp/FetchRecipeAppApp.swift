b8c255398a2f68eb084ec3f0e7912c511281654a
ClearlyAwesome <findsolutions2019@gmail.com>
2024-12-16 13:08:13 -0800

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by R C on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
